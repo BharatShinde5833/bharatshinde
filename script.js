7fa5d13be7aa3bf74dbe31b1925892c20677a5d5
BharatShinde5833 <91989829+BharatShinde5833@users.noreply.github.com>
2021-10-06 10:29:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+var text = document.getElementById("changable_text");
+var id = 0;
+setInterval(change_text, 2000);
+var i = 0;
+var j = 0;
+function change_text() {
+    if (i == 0) {
+        text.innerHTML = "a programer.";
+        i++;
+    }
+    else if (i == 1) {
+        text.innerHTML = "a student.";
+        i++;
+    }
+    else if (i == 2) {
+        text.innerHTML = "a proud indian.";
+        i++;
+    }
+	 else if (i == 3) {
+        text.innerHTML = "a Influencer.";
+        i++;
+    }
+    else if (i == 4) {
+        text.innerHTML = "a WEB DEvloper.";
+        i = 0;
+    }
+}
+var inputLable = document.getElementById('inputlable');
+var nextTime = 0;
+var progress = document.getElementById('progress');
+var inputbar = document.getElementById('inputbar');
+var nextIcon = document.getElementById('nextIcon');
+var index = document.getElementById("index");
+function next() {
+    if (nextTime == 0) {
+        inputLable.innerHTML = "Your name";
+        progress.style.width = "33%";
+        index.innerHTML="2/3";
+        nextTime++;
+    }
+    else if (nextTime == 1) {
+        inputLable.innerHTML = "Your email or phone number";
+        progress.style.width = "66%";
+        index.innerHTML="3/3";
+        nextTime++;
+    }
+    else if (nextTime == 2) {
+        progress.style.width = "100%";
+        nextIcon.innerHTML="submit";
+        nextTime++;
+    }
+    else if (nextTime == 3) {
+        index.innerHTML="";
+        inputbar.style.fontSize = "5vh";
+        inputbar.style.textAlign = "center";
+        inputbar.style.fontWeight = "500";
+        inputbar.style.opacity = "0.9";
+        inputbar.style.color = "skyblue";
+        progress.style.visibility = "hidden";
+        inputbar.style.textTransform="capitalize";
+        inputLable.innerHTML = " ";
+        inputbar.innerHTML = "Thank you! I'll contact you shortly.";
+        nextTime = 0;
+    }
+}
+
+
+var mypic= document.getElementById('my-pic');
+var picindex = document.getElementById('pic-index');
+var index=1;
+function nextPic(){
+	if(index==1){
+		mypic.src="bharat2.jpg";
+		picindex.innerHTML="2/4";
+		index++;
+	}
+	else if(index==2){
+		mypic.src="bharat3.jpg";
+		picindex.innerHTML="3/4";
+		index++;
+	}
+	else if(index==3){
+		mypic.src="bharat4.jpg";
+		picindex.innerHTML="4/4";
+		index++;
+	}
+	else{
+		mypic.src="bharat1.jpg";
+		picindex.innerHTML="1/4";
+		index=1;
+	}
+}
+function lastPic(){
+    if(index==3){
+		mypic.src="bharat2.jpg";
+		picindex.innerHTML="2/4";
+		index++;
+	}
+	else if(index==2){
+		mypic.src="bharat3.jpg";
+		picindex.innerHTML="3/4";
+		index++;
+	}
+	else if(index==1){
+		mypic.src="bharat4.jpg";
+		picindex.innerHTML="4/4";
+		index++;
+	}
+	else{
+		mypic.src="bharat.jpg";
+		picindex.innerHTML="1/4";
+		index=1;
+	}
+}
